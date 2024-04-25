cca6ecdb78623338a4cad2bbaf06923a0f61966f
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-04-26 00:19:18 +0530

    socket connection event

--- server/index.js
+++ server/index.js
@@ -5,8 +5,18 @@ import bodyParser from "body-parser";
 const app = express();
 const io = Server(); //creating the socket server
 
-app.use(bodyParser.json());
+const emailToSocketMapping = new Map();
 
+app.use(bodyParser.json());
+io.on("connection", (socket) => {
+  socket.on("join-room", (data) => {
+    const { emailId, roomId } = data;
+    console.log("User", emailId, "Joined Room ", roomId);
+    emailToSocketMapping.set(emailId, socket.id);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-join", { emailId }); //This means that all other clients in the same room will receive this event. The event data includes the emailId of the user who joined the room.
+  });
+});
 
 app.listen(8000, () => {
   console.log("Server is started on port 8000");
