1a0b76f82622665c44ee644fd8a3026804c9549d
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-05-02 01:16:13 +0530

    Emmiting incomming and accepted call event

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const io = new Server({
 }); //creating the socket server
 
 const emailToSocketMapping = new Map();
+const socketToEmailMapping = new Map();
 
 app.use(bodyParser.json());
 io.on("connection", (socket) => {
@@ -15,9 +16,23 @@ io.on("connection", (socket) => {
     const { emailId, roomId } = data;
     console.log("User", emailId, "Joined Room ", roomId);
     emailToSocketMapping.set(emailId, socket.id);
+    socketToEmailMapping.set(socket.id, emailId);
     socket.join(roomId);
-    socket.emit('joined-room',{roomId})
-    socket.broadcast.to(roomId).emit("user-join", { emailId }); //This means that all other clients in the same room will receive this event. The event data includes the emailId of the user who joined the room.
+    socket.emit("joined-room", { roomId });
+    socket.broadcast.to(roomId).emit("user-joined", { emailId }); //This means that all other clients in the same room will receive this event. The event data includes the emailId of the user who joined the room.
+  });
+
+  socket.on("call-user", (data) => {
+    const { emailId, offer } = data;
+    const fromEmail = socketToEmailMapping.get(socket.id);
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit("incomming-call", { from: fromEmail, offer });
+  });
+
+  socket.on("call-accepted", (data) => {
+    const { emailId, ans } = data;
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit("call-accepted",{ans})
   });
 });
 
